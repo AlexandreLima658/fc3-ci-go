02613bda383e5f88566886babf4fad5700c914b9
Carlos Alexandre <alexandrec@alu.ufc.br>
2024-10-17 14:05:46 -0300

    fix bug add

--- main_test.go
+++ main_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestSoma(t *testing.T) {
 
-	total := soma(12, 15)
+	total := soma(15, 15)
 
 	if total != 30 {
 		t.Errorf("Resultado esperado: %d. Resultado obtido: %d", total, 30)
